472654c21de352ab16e6b3744228936a317ffe2c
tlswlsdntks <tlswlsdntks@naver.com>
2024-12-18 10:06:14 +0900

    java-basic

--- src/extends1/super2/ClassC.java
+++ src/extends1/super2/ClassC.java
@@ -2,7 +2,7 @@ package extends1.super2;
 
 public class ClassC extends ClassB {
     public ClassC(int a) {
-        //super(); //java: constructor ClassC in class extends1.super2.ClassC cannot be applied to given types;
+        //super(); //constructor ClassC in class extends1.super2.ClassC cannot be applied to given types;
         super(a); // 또는 super(10, 20);
         System.out.println("ClassC 생성자");
     }
